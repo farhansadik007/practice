dfceeb78ae11fb57c9cb2fc5105a0cca1a6f5a47
Farhan Sadik <42910108+farhansadik007@users.noreply.github.com>
2022-11-29 16:33:03 +0600

    Create problem_4.c
    You are given an integer n . Now print n to 1 . Implement it using recursion.
    Sample Input :                       Sample Output :
    4                                             4 3 2 1

---
+++ module_25.5/problem_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void recursion(int n) {
+    printf("%d ", n);
+    if(n>1) recursion(n-1);
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    recursion(n);
+    return 0;
+}
