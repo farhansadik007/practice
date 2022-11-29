967d4107efd48d0a2b84fe91ecf94a6dd09e4c4c
Farhan Sadik <42910108+farhansadik007@users.noreply.github.com>
2022-11-29 15:20:33 +0600

    Create problem_2.c
    Write a C program to take one positive integer N, the size of an array as input. Then take a positive integer array
    of size N . And the next line will contain k . Now find the k-th largest and kth-smallest element from the array.
    Implement it using function. And try to implement it using 3 functions .1st one is for sorting , second one for
    finding k-th largest element and third one for finding the kth- smallest element.
    See the sample output for more clarification.
    Note – 1<=k<=N

---
+++ module_25.5/problem_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int sort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n - 1; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
+void largest(int arr[], int k, int n) {
+    printf("%d largest element = %d\n", k, arr[n-k]);
+}
+void smallest(int arr[], int k, int n) {
+    printf("%d smallest element = %d\n", k, arr[k-1]);
+}
+int main()
+{
+    int n, k;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    scanf("%d", &k);
+
+    sort(arr, n);
+    largest(arr, k, n);
+    smallest(arr, k, n);
+    return 0;
+}
